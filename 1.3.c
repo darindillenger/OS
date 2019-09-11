a1e843f3ef92bc922859601a5aec16cd664b8efb
darindillenger <55089041+darindillenger@users.noreply.github.com>
2019-09-11 19:06:28 -0400

    Update 1.3.c

--- 1.3.c
+++ 1.3.c
@@ -1,34 +1,45 @@
+/**
+ * This program satisfies Part 3 
+ * of Assignment 1.
+ *
+ * @author Darin Johnson
+ * @date September 11th, 2019
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
-//rmchr.c remove all occurrences of the character from the string.
-char* rmchr(char* str, char& c) {
-  printf("[enter string being used]: %s\n", str);
-  printf("[Remove the string]: %s\n", c);
-  printf("%c\n", str[0]);  
-  
-  for (int i =0; i < strlen(str); i++) { // visits everying in the array
-    if (c[0] == str[1]) {
-      
-      memmove(&str[i], &str[i + 1], strlen(str));
-      
-      
+
+
+char* rmchr(char* str, char* c) {
+
+
+    // Visits address in char array.
+    for (int i = 0; i < strlen(str); i++) {
+        if (c[0] == str[i]) {
+
+
+             //memmove. Shifts specified address
+
+            memmove(&str[i], &str[i + 1], strlen(str));
+
+
+            i--;
+        }
     }
-  }
-}
-return str;
+
+    return str; // returns the string word
 }
 
 int main (int argc, char** argv) {
-  char *str = argv[1];
-  char *c = (char*) argv[2];
-  
-  str = rmchr(str, c);
-  printf("Result of occurrences '%s'\n", str );
-  
-  rerun 0;
 
+    char *str = argv[1];
+    char *c = (char*) argv[2];
 
+    str = rmchr(str, c);
+    printf("Result '%s'\n", str ); // prince the whats was removed
+
+    return 0;
 }
